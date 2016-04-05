1ebeb2354c4122d435f7f5e4293dbed414b7bc7d
matangilead <0066789Aa>
2016-04-05 08:14:13 -0700

    printing to terminal every audit event on relevant key/file.

--- daemon.py
+++ daemon.py
@@ -1,24 +1,55 @@
 import win32evtlog
+import ConfigParser
+import os
 if __name__ == '__main__':
-                    server = 'localhost' # name of the target computer to get event logs
-                    logtype = 'System'
-                    hand = win32evtlog.OpenEventLog(server,logtype)
-                    flags = win32evtlog.EVENTLOG_FORWARDS_READ|win32evtlog.EVENTLOG_SEQUENTIAL_READ
-                    total = win32evtlog.GetNumberOfEventLogRecords(hand)
-                    while True:
-                        events = win32evtlog.ReadEventLog(hand, flags,0)
-                        if events:
-                            for event in events:
-                                    print 'Event Category:', event.EventCategory
-                                    print 'Time Generated:', event.TimeGenerated
-                                    print 'Source Name:', event.SourceName
-                                    print 'Event ID:', event.EventID
-                                    print 'Event Type:', event.EventType
-                                    data = event.StringInserts
-                                    if data:
-                                        print 'Event Data:'
-                                        for msg in data:
-                                            print msg
-                                            break
+    #read ini file
+    main_conf_path = r".\iAmVM_conf.ini"
+    main_conf = ConfigParser.ConfigParser()
+    main_conf.read(main_conf_path)
 
+    #create a list of the filenames and keys
+    files_name_file = (main_conf.get("Paths", "fileNames"))
+    reg_keys_filename = (main_conf.get("Paths", "regKeysPath"))
+    files_name = open(files_name_file, 'r')
+    reg_keys = open(reg_keys_filename, 'r')
+    known_list=[]
+    for file in files_name:
+        file=file.rstrip()
+        name=os.path.basename(file)
+        known_list.append(name)
+    for file in reg_keys:
+        file=file.rstrip()
+        name=os.path.basename(file)
+        known_list.append(name)
 
+    #define event listener
+    server = 'localhost'
+    logtype = 'Security'
+    hand = win32evtlog.OpenEventLog(server,logtype)
+    flags = win32evtlog.EVENTLOG_FORWARDS_READ|win32evtlog.EVENTLOG_SEQUENTIAL_READ
+    total = win32evtlog.GetNumberOfEventLogRecords(hand)
+    print known_list
+    while True:
+        events = win32evtlog.ReadEventLog(hand, flags,0)
+        if events:
+            for event in events:
+                id=event.EventID
+                if (id==4656 or id==4664):
+                        data_list = event.StringInserts
+                        if (data_list):
+                           for file in known_list:
+                               for data in data_list:
+                                   if file in data:
+                                        print 'Event Category:', event.EventCategory
+                                        print 'Time Generated:', event.TimeGenerated
+                                        print 'Source Name:', event.SourceName
+                                        print 'Event ID:', event.EventID
+                                        print 'Event Type:', event.EventType
+                                        processType=data_list[5]
+                                        processLocation=data_list[6]
+                                        processCalled=data_list[len(data_list)-2]
+                                        print "Kind: {}".format(processType)
+                                        print "Process that accessed: {}".format(processLocation)
+                                        print "Process initiated the call: {}".format(processCalled)
+                                        print "***********************"
+                                        break
