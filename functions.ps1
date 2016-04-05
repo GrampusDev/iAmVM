a7e7ffef670123ac003e4d548ff47d8fb95c6f43
matangilead <0066789Aa>
2016-04-05 05:05:46 -0700

    deleted strange chars

--- functions.ps1
+++ functions.ps1
@@ -3,20 +3,20 @@ function Get-IniContent ($filePath)
     $ini = @{}
     switch -regex -file $FilePath
     {
-        “^\[(.+)\]” # Section
+        "^\[(.+)\]" # Section
         {
             $section = $matches[1]
             $ini[$section] = @{}
             $CommentCount = 0
         }
-        “^(;.*)$” # Comment
+        "^(;.*)$" # Comment
         {
             $value = $matches[1]
             $CommentCount = $CommentCount + 1
             $name = “Comment” + $CommentCount
             $ini[$section][$name] = $value
         }
-        “(.+?)\s*=(.*)” # Key
+        "(.+?)\s*=(.*)" # Key
         {
             $name,$value = $matches[1..2]
             $ini[$section][$name] = $value
@@ -34,7 +34,7 @@ function AddAuditToFile {
     $path=Resolve-Path($path)
     Get-Acl $path -Audit | Format-List Path,AuditToString | Out-File -FilePath 'file_before.txt' -Width 200 -Append
     $File_ACL = Get-Acl $path
-    $AccessRule = New-Object System.Security.AccessControl.FileSystemAuditRule("Everyone","Read�,"none","none",�Success")
+    $AccessRule = New-Object System.Security.AccessControl.FileSystemAuditRule("Everyone","Read","none","none","Success")
     $File_ACL.AddAuditRule($AccessRule)
     $File_ACL | Set-Acl $path
     Get-Acl $path -Audit | Format-List Path,AuditToString | Out-File -FilePath 'file_after.txt' -Width 200 -Append
@@ -44,7 +44,7 @@ function AddAuditToFile {
         $parentFolder=$fileObject.Directory
         Get-Acl $parentFolder -Audit | Format-List Path,AuditToString | Out-File -FilePath 'file_before.txt' -Width 200 -Append
         $File_ACL = Get-Acl $parentFolder
-        $AccessRule = New-Object System.Security.AccessControl.FileSystemAuditRule("Everyone","ListDirectory�,"none","none",�Success")
+        $AccessRule = New-Object System.Security.AccessControl.FileSystemAuditRule("Everyone","ListDirectory","none","none","Success")
         $File_ACL.AddAuditRule($AccessRule)
         $File_ACL | Set-Acl $parentFolder
         Get-Acl $parentFolder -Audit | Format-List Path,AuditToString | Out-File -FilePath 'file_after.txt' -Width 200 -Append
@@ -65,7 +65,7 @@ function DelAuditFromFile {
     $path=Resolve-Path($path)
     Get-Acl $path -Audit | Format-List Path,AuditToString | Out-File -FilePath 'file_before_delete.txt' -Width 200 -Append
     $File_ACL = Get-Acl $path
-    $AccessRule = New-Object System.Security.AccessControl.FileSystemAuditRule("Everyone","Read�,"none","none",�Success")
+    $AccessRule = New-Object System.Security.AccessControl.FileSystemAuditRule("Everyone","Read","none","none","Success")
     $File_ACL.RemoveAuditRule($AccessRule)
     $File_ACL | Set-Acl $path
     Get-Acl $path -Audit | Format-List Path,AuditToString | Out-File -FilePath 'file_after_delete.txt' -Width 200 -Append
@@ -74,7 +74,7 @@ function DelAuditFromFile {
     $parentFolder=(get-item $path).Directory.FullName
     Get-Acl $parentFolder -Audit | Format-List Path,AuditToString | Out-File -FilePath 'file_before.txt' -Width 200 -Append
     $File_ACL = Get-Acl $parentFolder
-    $AccessRule = New-Object System.Security.AccessControl.FileSystemAuditRule("Everyone","ListDirectory�,"none","none",�Success,Fail")
+    $AccessRule = New-Object System.Security.AccessControl.FileSystemAuditRule("Everyone","ListDirectory","none","none","Success,Fail")
     $File_ACL.RemoveAuditRule($parentFolder)
     $File_ACL | Set-Acl $parentFolder
     Get-Acl $parentFolder -Audit | Format-List Path,AuditToString | Out-File -FilePath 'file_after.txt' -Width 200 -Append
@@ -89,7 +89,7 @@ function AddAuditToRegKey {
 
     Get-Acl $key -Audit | Format-List Path,AuditToString | Out-File -FilePath 'reg_before.txt' -Width 200 -Append
     $RegKey_ACL = Get-Acl $key
-    $AccessRule = New-Object System.Security.AccessControl.RegistryAuditRule("Everyone","ReadKey�,"none","none",�Success")
+    $AccessRule = New-Object System.Security.AccessControl.RegistryAuditRule("Everyone","ReadKey","none","none","Success")
     $RegKey_ACL.AddAuditRule($AccessRule)
     $RegKey_ACL | Set-Acl $key
     Get-Acl $key -Audit | Format-List Path,AuditToString | Out-File -FilePath 'reg_after.txt' -Width 200 -Append
@@ -105,7 +105,7 @@ function DelAuditFromRegKey {
 
     Get-Acl $key -Audit | Format-List Path,AuditToString | Out-File -FilePath 'reg_before_delete.txt' -Width 200 -Append
     $RegKey_ACL = Get-Acl $key
-    $AccessRule = New-Object System.Security.AccessControl.RegistryAuditRule("Users","ReadKey�,"none","none",�Success")
+    $AccessRule = New-Object System.Security.AccessControl.RegistryAuditRule("Users","ReadKey","none","none","Success")
     $RegKey_ACL.RemoveAuditRule($AccessRule)
     $RegKey_ACL | Set-Acl $key
     Get-Acl $key -Audit | Format-List Path,AuditToString | Out-File -FilePath 'reg_after_delete.txt' -Width 200 -Append
